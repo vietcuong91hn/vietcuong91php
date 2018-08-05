cd18e376384360286f88e4eef5a5ef74df94b233
Nguyen Viet Cuong <nguyenvietcuong91hn@gmail.com>
2018-08-05 20:11:58 +0700

    session

---
+++ Session/admin.php
@@ -0,0 +1,6 @@
+<?php
+echo '<br>' . __FILE__;
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+exit;
--- Session/index.php
+++ Session/index.php
@@ -19,25 +19,12 @@ session_start();
 <body>
 <div class="container">
 
-    <?php
-
-    // Set session variables
-    $_SESSION["username"] = "cuong";
-    $_SESSION["address"] = "ha noi";
-    echo "Da tao session";
-
-
-    echo "<pre>";
-    print_r($_SESSION);
-    echo "</pre>";
-    ?>
-
-    <form class="form-signin" style="margin: 0 auto; width: 500px">
+    <form name="login" action="http://localhost/vietcuong91php/session/admin.php" class="form-signin" style="margin: 0 auto; width: 500px" method="post">
         <h2 class="form-signin-heading">Please sign in</h2>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox">
             <label>
                 <input type="checkbox" value="remember-me"> Remember me
